55c5690d62238c6a1eb0f4e9636b0e3b970ec102
Erpheus <erpheus@gmail.com>
2013-07-13 16:53:54 +0200

    fronted not connected to backend yet

--- js/project.js
+++ js/project.js
@@ -1,8 +1,11 @@
-var project_id;
-
 $$('document').ready(function(){
 
-    project_id = sp_urlParams["project_id"];
+    function validateEmail(email) { 
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    var project_id = sp_urlParams["project_id"];
     
     var url = sp_server_url+'/project/'+project_id;
 
@@ -21,11 +24,57 @@ $$('document').ready(function(){
         
             var project = json;
 
-            
+
         }
     }
 
     // AJAX callback
-    Lungo.Service.get(url, data, success, dataType);
+    //Lungo.Service.get(url, data, success, dataType);
+    
+    //Money Stepper
+
+    var changeMoney = function(delta){
+        var actualNumber = parseFloat($$("#moneyInput").val().replace(",","."));
+        $$("#moneyInput").val(Math.max(0,(actualNumber+delta).toFixed(2))+"€");
+    }
+
+    $$('#plusButton').tap(function(){
+        changeMoney(0.2);
+    });
+
+    $$('#minusButton').tap(function(){
+        changeMoney(-0.2);
+    });
+
+    //User image getter
+
+    function searchImages(){
+        function gravatarImage(email){
+            email = email.toLowerCase().trim();
+            return 'http://www.gravatar.com/avatar/' + md5(email) + '?s=200';
+        }
+
+        var src;
+        if (validateEmail($$("#emailInput").val())) {
+            src = gravatarImage($$("#emailInput").val());
+            $$("#userImage").css("width","200px");
+            $$("#userImage").css("height","200px");
+        }else{
+            src = "http://"+($$("#emailInput").val().replace(" ","."))+".jpg.to";
+            $$("#userImage").css("max-width","200px");
+            $$("#userImage").css("max-height","200px");
+        }
+
+        $$("#userImage").attr("src",src);
+        $$("#userImage").css("visibility","visible");
+        
+
+    };
+
+    $$('#searchButton').tap(function(){
+        searchImages();
+    });
+
+
 
 });
